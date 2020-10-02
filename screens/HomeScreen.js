a950f8ef0dc3ccc091bb48e66f701fffccd946b1
joannayu98 <joannayu2021@u.northwestern.edu>
2020-10-01 21:02:22 -0500

    attempted authentication

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,21 +3,56 @@ import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import {Button, Card, Title, Paragraph, TextInput} from 'react-native-paper';
 import Banner from '../components/Banner.js';
+import { AsyncStorage } from 'react-native';
+import { getRedirectUrl, useAuthRequest, makeRedirectUri } from 'expo-auth-session';
+makeRedirectUri()
+console.log("this is the redirect url: " + getRedirectUrl())
 
+_initData = async () => {
+  try {
+    await AsyncStorage.setItem(
+      '@Friends:key',
+      'Joe'
+    );
+  } catch (error) {
+    // Error saving data
+  }
+};
+
+const AddFriend = (friend) => {
+  try {
+    const value = await AsyncStorage.getItem('Friends');
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+      try {
+        await AsyncStorage.setItem(
+          '@Friends:key',
+          value,
+          friend
+        );
+      } catch (error) {
+      }
+    }
+  } catch (error) {
+    // Error retri
+  } 
+}
 
 const HomeScreen = ({navigation}) => {
+  const [friend, setFriend] = React.useState("")
   return (
     <SafeAreaView style={styles.container}>
         <Banner navigation={navigation} />
         <Card style={styles.card}>
           <Card.Title
-            title="Search for friends"
+            title = "Friends"
             subtitle="Input your friends' Spotify User ID"></Card.Title>
           <Card.Content>
-            <TextInput label="User ID"></TextInput>
+            <TextInput label="User ID" onChangeText={(value) => setFriend(value)}></TextInput>
           </Card.Content>
           <Card.Actions>
-            <Button>Search</Button>
+            <Button onPress={() => AddFriend(friend)} >Search</Button>
           </Card.Actions>
         </Card>
       {/* <StatusBar style="auto" /> */}
@@ -30,6 +65,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignContent: 'flex-start',
+    justifyContent: 'center',
   },
   card: {
     width: '100%',
