c91962efcfffaa00227b7808bc6175043cac0156
jrzax <jrzax@aol.com>
2020-10-15 22:53:52 -0500

    Minor bug to generate playlists screen

--- screens/GeneratePlaylistScreen.js
+++ screens/GeneratePlaylistScreen.js
@@ -7,7 +7,7 @@ const GeneratePlaylistScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.cardContainer}>
-        <Button title='Generate Playlist' onPress={() => navigation.navigate('GeneratePlaylistForm')}/>
+        <Button title='Henerate Playlist' onPress={() => navigation.navigate('GeneratePlaylistForm')}/>
       </View>
     </SafeAreaView>
   );
