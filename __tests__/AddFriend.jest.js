527a0403d174b70419c08fbb7117f8dfacb1f3b4
jrzax <jrzax@aol.com>
2020-10-26 22:21:04 -0500

    travis

--- __tests__/AddFriend.jest.js
+++ __tests__/AddFriend.jest.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {cleanup, fireEvent, render} from '@testing-library/react';
 import AddFriendSearch from '../components/AddFriendSearch';
 import getTokens from "../spotifyAuth/getAccessToken";
-import {describe, expect, it} from '@jest/globals';
-import {mockFirebase, mockCollection} from 'firestore-jest-mock/mocks/firestore.js';
+import {describe, expect, it} from '@jest/globals'
 
+
+const {mockCollection } = require('firestore-jest-mock/mocks/firestore');
+const {mockFirebase} = require('firestore-jest-mock');
 describe('Add Friend Tests', () => {
     mockFirebase({
         database: {
@@ -14,11 +16,12 @@ describe('Add Friend Tests', () => {
 
     it('Lookup User ID', async () => {
         const userid = 'jrzax';
-        const {getByText, getByPlaceholder} = render(
+        const screen = render(
             <AddFriendSearch/>
         );
-        const button = getByText(/Add Friend/i);
-        fireEvent.changeText(getByPlaceholder(/Friend's Spotify User ID/i), userid);
+        const button = screen.getByText(/Add Friend/i);
+        console.log(screen)
+        fireEvent.change(screen.getByLabelText(/Friend's Spotify User ID/i), userid);
         fireEvent.press(button);
         
         const access = await getTokens();
