80f6d33779352377e38abdd4a63329b4570d47c9
jrzax <jrzax@aol.com>
2020-12-07 18:51:25 -0600

    Added user login test

--- cypress/integration/examples/App.spec.js
+++ cypress/integration/examples/App.spec.js
@@ -1,5 +1,17 @@
+const { validateYupSchema } = require("formik");
+
+//after running a test, you have to logout from inside cypress to run again
+// (or else you automatically sign in before tests run)
 describe ('Test App', () => {
-    it ('launches', () => {
-      cy.visit ('/');
+    it ('User logs in', () => {
+      cy.visit('/')
+      cy.get('input[type="email"]').as("email");
+      cy.get('input[type="password"]').as("password")
+      //data-focusable is touchable opacity
+      cy.get('[data-focusable="true"]').contains(/log in/i).as("login")
+
+      cy.get("@email").type("testuser@fake.com")
+      cy.get("@password").type("testtest")
+      cy.get("@login").click()
     });
   });
