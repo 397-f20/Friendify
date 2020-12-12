a135fe5b79884ac19ac60f87d43f13d7541ae757
Grace Richardson <gracerichardson2021@u.northwestern.edu>
2020-12-12 21:49:16 +0000

    Jest Test Grace

---
+++ __tests__/FriendPlaylistTest.jest.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import FriendPlaylists from '../components/FriendPlaylists';
+import getTokens from "../spotifyAuth/getAccessToken";
+import {describe, expect, it} from '@jest/globals'
+import {useNavigation} from '@react-navigation/native'
+import FriendPlaylistButton from '../components/FriendPlaylistButton';
+
+afterEach(cleanup);
+describe ('Friend Playlist Test'),() => {
+    it('List Renders', async() => {
+        const screen = render(
+            <FriendPlaylistButton />
+          );
+    }   
+    );
+
+}
