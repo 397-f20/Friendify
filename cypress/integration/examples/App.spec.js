11de6fe023e0f8fc04b1ddaa9875988d5026184f
Grace Richardson <gracerichardson2021@u.northwestern.edu>
2020-12-15 01:11:05 +0000

    breaking cypress check

--- cypress/integration/examples/App.spec.js
+++ cypress/integration/examples/App.spec.js
@@ -13,7 +13,7 @@ describe ('Test App', () => {
       cy.get("@email").type("testuser@fake.com")
       cy.get("@password").type("testtest")
       cy.get("@login").click()
-      cy.wait(10000);
+    
       cy.get('[data-focusable="true"]').contains(/logout/i).as("logout")
       cy.get("@logout").click()
       
